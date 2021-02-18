46a092a713749f2966a2df66e6289bea518ab1b3
Chih-Kai Ting <tingken0214@gmail.com>
2021-02-18 15:14:05 +0800

    feat(simple-scene-classifier): use knn classification instead

---
+++ motion_marmot/simple_scene_classifier/simple_scene_classifier.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+
+
+class SimpleSceneClassifier:
+    """
+    Simple Scene Classifier
+    Description: Basically using K-Nearest Neighbors to train the classifier with the data 
+    of several scene motion mask attributes
+    """
+
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return f"SimpleSceneClassifier(name={self.name})"
+
+    def data_extraction(self, file_name):
+        """Extract Data from CSV File"""
+        data = pd.read_csv(file_name)
+        x = data.iloc[:, :-1].values
+        y = data['scene']
+        return x, y
+
+    def train_model(self, train_x, train_y):
+        return KNeighborsClassifier(n_neighbors=4).fit(train_x, train_y)
+
+    def save_model(self, model, model_name):
+        from joblib import dump
+        dump(model, model_name)
